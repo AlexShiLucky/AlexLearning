5c7139336c3e4f0c51e109ff0229d5649e36948d
Alex Shi <shiweining123@gmail.com>
2023-04-27 14:13:18 +0800

    [新增] 1.添加class实例.
    Signed-off-by: Alex Shi <shiweining123@gmail.com>

---
+++ OOP/OOP-Class/class_misc/class_x2.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#if ASSERT_EN
+#define ASSERT(assertion) do { if(!(assertion)) \
+    printf("(%s) assert failed at %s:%d \r\n", #assertion, __FUNCTION__, __LINE__); } while(0)
+#else  /* ASSERT_EN */
+#define ASSERT(assertion)
+#endif /* ASSERT_EN */
+
+/*  抽象父类  */
+struct parent_class {
+    int a;
+    /*  反映不同类别属性的方法  */
+    void (*vfunc)(struct parent_class* self, int a);
+};
+
+/*  抽象父类的方法调用  */
+void parent_class_vfunc(struct parent_class *self, int a)
+{
+    ASSERT(self != NULL);
+    ASSERT(slef->vfunc != NULL);
+
+    /*  调用对象本身的虚拟函数  */
+    self->vfunc(self, a);
+}
+
+/*  抽象类的虚拟函数  */
+static void _parent_vfunc(struct parent_class* self, int a)
+{
+    ASSERT(self != NULL);
+
+    self->a += a;
+    /*  输出相应的信息  */
+    printf("parent's a=%d\n", self->a);
+}
+
+/*  抽象父类的构造函数,将成员变量赋值和虚拟函数绑定实现  */
+void parent_class_init(struct parent_class* self)
+{
+    ASSERT(self != NULL);
+
+    self->a = 1;
+    self->vfunc = _parent_vfunc;
+}
+
+/*  继承自 parent_class 的子类  */
+struct child_class
+{
+    struct parent_class parent;
+    int b;
+};
+
+/*  子类的虚拟函数实现  */
+static void _child_class_vfunc(struct parent_class* self, int a)
+{
+    struct child_class *child = (struct child_class*)self;
+    child->b = a + 10;
+    /*  输出相应的信息  */
+    printf("child's b=%d\n", child->b);
+}
+
+/*  子类的构造函数  */
+void child_class_init(struct child_class* self)
+{
+    struct parent_class* parent;
+    /*  强制类型转换获得父类指针  */
+    parent = (struct parent_class*) self;
+    ASSERT(parent != NULL);
+    /*  设置子类的虚拟函数  */
+    parent->vfunc = _child_class_vfunc;
+}
+
+void obj_test()
+{
+    struct parent_class *ptr;
+
+    struct parent_class parent;
+    struct child_class  child_a;
+    struct child_class  child_b;
+    struct child_class  child_c;
+
+    parent_class_init(&parent);
+    child_class_init(&child_a);
+    child_class_init(&child_b);
+    child_class_init(&child_c);
+
+    ptr = (struct parent_class *)&parent;
+    parent_class_vfunc(ptr, 10);
+
+    ptr = (struct parent_class *)&child_a.parent;
+    parent_class_vfunc(ptr, 20);
+
+    ptr = (struct parent_class *)&child_b.parent;
+    parent_class_vfunc(ptr, 30);
+
+    ptr = (struct parent_class *)&child_c.parent;
+    parent_class_vfunc(ptr, 40);
+}
+
+int main(void)
+{
+    obj_test();
+    return 0;
+}
+
+
---
+++ OOP/OOP-Class/class_misc/class_x3.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+// 父类
+struct Shape {
+    void (*draw)(void);
+    void (*erase)(void);
+    void (*move)(void);
+    int  (*getColor)(void);
+    void (*setColor)(int);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 子类
+struct Circle {
+    struct Shape shape;
+    const char *name;
+};
+
+static void circle_draw(void)
+{
+    printf("circle draw\n");
+}
+
+static void circle_erase(void)
+{
+    printf("circle erase\n");
+}
+
+static void circle_move(void)
+{
+    printf("circle move\n");
+}
+
+static struct Circle   c = {
+    .shape = {
+        .draw  = circle_draw,
+        .erase = circle_erase,
+        .move  = circle_move
+    },
+    .name = "Circle"
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 子类
+struct Square {
+    struct Shape shape;
+    const char *name;
+};
+
+static void square_draw(void)
+{
+    printf("square draw\n");
+}
+
+static void square_erase(void)
+{
+    printf("square erase\n");
+}
+
+static void square_move(void)
+{
+    printf("square move\n");
+}
+
+static struct Square   s = {
+    .shape = {
+        .draw  = square_draw,
+        .erase = square_erase,
+        .move  = square_move
+    },
+    .name = "Square"
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 子类
+struct Triangle {
+    struct Shape shape;
+    const char *name;
+};
+
+static void triangle_draw(void)
+{
+    printf("triangle draw\n");
+}
+
+static void triangle_erase(void)
+{
+    printf("triangle erase\n");
+}
+
+static void triangle_move(void)
+{
+    printf("triangle move\n");
+}
+
+static struct Triangle t = {
+    .shape = {
+        .draw  = triangle_draw,
+        .erase = triangle_erase,
+        .move  = triangle_move
+    },
+    .name = "Triangle"
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 统一调用接口 */
+void doStuff(struct Shape *s)
+{
+    s->erase();
+    s->draw();
+    s->move();
+}
+
+int main(void)
+{
+    doStuff((struct Shape *)&c);
+    doStuff((struct Shape *)&s);
+    doStuff((struct Shape *)&t);
+
+    return 0;
+}
+
